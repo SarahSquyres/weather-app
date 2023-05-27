ae14b2262cc76a403164e16297c5d5055e7bc4f5
Sarah Squyres <ssquyres15@gmail.com>
2023-05-27 17:54:21 -0500

    added five day forecast

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,13 @@ $(function () {
     // Selectors
     var searchBtn = $('#search-btn')
     var apiKey = '4002166d0bd05a797585e40fd9241a1b'
+    
 
     // get city name from user
     searchBtn.on('click', function () {
         var cityName = $('#search-input').val().trim();
-        searchWeather(cityName)
-
+        searchWeather(cityName);
+        getForecast(cityName);
     })
 
     // search weather & console.log data
@@ -20,18 +21,32 @@ $(function () {
             .then(function (data) {
                 console.log(data);
                 var city = data.city.name;
+                var date = data.list[0].dt_txt;
                 var temp = data.list[0].main.temp;
                 var wind = data.list[0].wind.speed;
                 var humidity = data.list[0].main.humidity;
-                $("<h1>" + city + "</h1>").appendTo("#city");
-                $("<h2> Temp: "  + temp + "</h2>").appendTo("#temp");
-                $("<h2> Wind Speed: " + wind + "</h2>").appendTo("#wind-speed");
-                $("<h2> Humidity: " + humidity + "</h2>").appendTo("#humidity");
+                $("<h1>" + city + " " + date + "</h1>").appendTo("#city");
+                $("<h2> Temp: "  + temp + "°F" + "</h2>").appendTo("#temp");
+                $("<h2> Wind Speed: " + wind + "MPH" +"</h2>").appendTo("#wind-speed");
+                $("<h2> Humidity: " + humidity + "%" +"</h2>").appendTo("#humidity");
                 
             });
+
     }
 
-
+    function getForecast(city) {
+        
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var dataList = data.list;
+            for (var i = 0; i < dataList.length; i+=8) {
+              $("<li>").text(`${dataList[i].dt_txt}: Temp - ${data.list[i].main.temp}°F  Wind Speed - ${data.list[i].wind.speed}MPH  Humidity - ${data.list[i].main.humidity}%`).appendTo("#five-day-forecast");
+            }
+          });
+      }
 
 
 });

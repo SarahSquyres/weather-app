0861f0b1bd48736f4e5ff221dd46d8caaf1ef6cd
Sarah Squyres <ssquyres15@gmail.com>
2023-05-28 23:54:40 -0500

    save search to local storage and display on button elements, alert box if input is empty

--- assets/script.js
+++ assets/script.js
@@ -3,13 +3,25 @@ $(function () {
     // Selectors
     var searchBtn = $('#search-btn')
     var apiKey = '4002166d0bd05a797585e40fd9241a1b'
-    
+    var clearBtn = $('#clear-btn')
+
     // get city name from user
     searchBtn.on('click', function () {
         var cityName = $('#search-input').val().trim();
         searchWeather(cityName);
         getForecast(cityName);
-        saveCity(cityName);
+        pushSearches();
+        $('#search-input').val("");
+
+        if (!cityName) {
+            alert("Please enter a city");
+          }
+      
+    })
+
+    clearBtn.on('click', function () {
+        localStorage.clear();
+        $('#search-list').hide();
     })
 
     // Search weather & console.log data
@@ -26,30 +38,52 @@ $(function () {
                 var wind = data.list[0].wind.speed;
                 var humidity = data.list[0].main.humidity;
                 $("<h1>" + city + " " + date + "</h1>").appendTo("#city");
-                $("<h2> Temp: "  + temp + "°F" + "</h2>").appendTo("#temp");
-                $("<h2> Wind Speed: " + wind + "MPH" +"</h2>").appendTo("#wind-speed");
-                $("<h2> Humidity: " + humidity + "%" +"</h2>").appendTo("#humidity");
+                $("<h2> Temp: " + temp + "°F" + "</h2>").appendTo("#temp");
+                $("<h2> Wind Speed: " + wind + "MPH" + "</h2>").appendTo("#wind-speed");
+                $("<h2> Humidity: " + humidity + "%" + "</h2>").appendTo("#humidity");
+            });
+    }
+    
+    // Five day forecast, help from Bard
+    function getForecast(city) {
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                var dataList = data.list;
+                for (var i = 0; i < dataList.length; i += 8) {
+                    $("<li>").text(`${dataList[i].dt_txt}: Temp - ${data.list[i].main.temp}°F  Wind Speed - ${data.list[i].wind.speed}MPH  Humidity - ${data.list[i].main.humidity}%`).appendTo("#five-day-forecast");
+                }
             });
     }
 
-    // Five day forecast
-    function getForecast(city) {
-        fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            var dataList = data.list;
-            for (var i = 0; i < dataList.length; i+=8) {
-              $("<li>").text(`${dataList[i].dt_txt}: Temp - ${data.list[i].main.temp}°F  Wind Speed - ${data.list[i].wind.speed}MPH  Humidity - ${data.list[i].main.humidity}%`).appendTo("#five-day-forecast");
-            }
-          });
-      }
+    // help from Bard
+    function pushSearches() {
+        var cityName = $('#search-input').val().trim();
+        //   check to see if cityName is empty
+        if (cityName === '') {
+            return;
+        } 
+        //   get city search list from local storage, convert JSON string to object, if local storage is empty, will set variable to empty array
+        var city = JSON.parse(localStorage.getItem("city")) || [];
 
-    // Save searches
-    function saveCity (city) {
-        localStorage.setItem(city, $('#search-input').val().trim())
+        // convert object to array in order to push variable to array (you can only push arrays to arrays)
+        cityArray = Array.from(city);
+        // push new input to local storage
+        cityArray.push(cityName);
+        localStorage.setItem("city", JSON.stringify(cityArray));
+        renderCityBtn();
     }
+
+    function renderCityBtn() {
+        for (var i = 0; i < cityArray.length; i++) {
+            var cityBtn = $("<button>");
+            cityBtn.text(cityArray[i]);
+            cityBtn.appendTo("#search-list");
+        }
+    }
+
 
 
 });
